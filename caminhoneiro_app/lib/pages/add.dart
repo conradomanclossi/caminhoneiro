e3f1d5f4e0a55b5a7eaf68fd510d346707bd9855
Conrado <conrado.macalossi@gmail.com>
2019-09-09 16:44:06 -0300

    Add FormField

--- caminhoneiro_app/lib/pages/add.dart
+++ caminhoneiro_app/lib/pages/add.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 showAdd(BuildContext context) {
   /// Popup Add
-  Container add = Container(
+  Container _add = Container(
       margin:
           EdgeInsets.only(top: 300.0, bottom: 300.0, left: 20.0, right: 20.0),
       child: Scaffold(
@@ -15,7 +15,10 @@ showAdd(BuildContext context) {
                     flex: 1,
                     child: GestureDetector(
                         onTap: () {
-                          print("Adicionar Faturamento");
+                          Navigator.of(context).pop();
+                          faturamentoAdd(context);
+
+                          //print("Adicionar Faturamento");
                         },
                         child: Container(
                             color: Colors.lightGreen,
@@ -36,17 +39,16 @@ showAdd(BuildContext context) {
                           print("Adicionar Custo");
                         },
                         child: Container(
-                          color: Colors.red,
-                          height: 100,
+                            color: Colors.red,
+                            height: 100,
                             child: Center(
                                 child: Text(
-                                  "Adicionar Custo",
-                                  textScaleFactor: 2.0,
-                                  style: TextStyle(
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.bold),
-                                ))
-                        ))),
+                              "Adicionar Custo",
+                              textScaleFactor: 2.0,
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold),
+                            ))))),
                 Expanded(
                     flex: 1,
                     child: GestureDetector(
@@ -54,17 +56,16 @@ showAdd(BuildContext context) {
                           print("Adicionar Despesa Pessoal");
                         },
                         child: Container(
-                          color: Colors.orange,
-                          height: 100,
+                            color: Colors.orange,
+                            height: 100,
                             child: Center(
                                 child: Text(
-                                  "Adicionar Despesa Pessoal",
-                                  textScaleFactor: 2.0,
-                                  style: TextStyle(
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.bold),
-                                ))
-                        )))
+                              "Adicionar Despesa Pessoal",
+                              textScaleFactor: 2.0,
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold),
+                            )))))
               ],
             ),
           )));
@@ -73,8 +74,61 @@ showAdd(BuildContext context) {
   showDialog(
     context: context,
     builder: (BuildContext context) {
-      return add;
+      return _add;
     },
   );
 }
 
+faturamentoAdd(BuildContext context) {
+  Container _faturamentoAdd = Container(
+      margin:
+          EdgeInsets.only(top: 225.0, bottom: 225.0, left: 20.0, right: 20.0),
+      child: Scaffold(
+          backgroundColor: Color(0xFFFFFF),
+          body: ClipRRect(
+              borderRadius: BorderRadius.circular(20.0),
+              child: Container(
+                  color: Colors.white,
+                  child: Center(
+                      child: Column(children: <Widget>[
+                    Container(
+                        margin: EdgeInsets.only(
+                            top: 30.0, bottom: 10.0, left: 20.0, right: 20.0),
+                        child: TextFormField(
+                          cursorColor: Colors.white,
+                          decoration: InputDecoration(hintText: 'Tipo'),
+                          maxLength: 60,
+                        )),
+                    Container(
+                        margin: EdgeInsets.only(
+                            top: 10.0, bottom: 10.0, left: 20.0, right: 20.0),
+                        child: TextFormField(
+                          cursorColor: Colors.white,
+                          decoration: InputDecoration(hintText: 'Frete'),
+                          maxLength: 60,
+                        )),
+                    Container(
+                        margin: EdgeInsets.only(
+                            top: 10.0, bottom: 10.0, left: 20.0, right: 20.0),
+                        child: TextFormField(
+                          cursorColor: Colors.white,
+                          decoration: InputDecoration(hintText: 'Valor'),
+                          maxLength: 60,
+                        )),
+                    Container(
+                        margin: EdgeInsets.only(
+                            top: 10.0, bottom: 30.0, left: 20.0, right: 20.0),
+                        child: RaisedButton(
+                          onPressed: () {},
+                          child: const Text('Salvar',
+                              style: TextStyle(fontSize: 20)),
+                        ))
+                  ]))))));
+  // exibe o dialog
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return _faturamentoAdd;
+    },
+  );
+}
