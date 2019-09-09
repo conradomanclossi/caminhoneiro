ba751d9c3cc2dc1a796053d18e7ffee924ae42f8
Conrado <conrado.macalossi@gmail.com>
2019-09-09 14:59:31 -0300

    Add Popup Visual

--- caminhoneiro_app/lib/pages/add.dart
+++ caminhoneiro_app/lib/pages/add.dart
@@ -3,46 +3,71 @@ import 'package:flutter/material.dart';
 showAdd(BuildContext context) {
   /// Popup Add
   Container add = Container(
-      margin: EdgeInsets.only(top: 300.0, bottom: 300),
-      decoration: BoxDecoration(borderRadius: BorderRadius.circular(20.0)),
-      child: Column(children: <Widget>[
-        Padding(
-          padding: EdgeInsets.all(0.0),
-          child: SizedBox(
-              width: 250,
-              height: 60,
-              child: RaisedButton(
-                color: Colors.lightGreen,
-                onPressed: () {},
-                child: const Text('Faturamento',
-                    style: TextStyle(fontSize: 30, color: Colors.white)),
-              )),
-        ),
-        Padding(
-          padding: EdgeInsets.all(0.0),
-          child: SizedBox(
-              width: 250,
-              height: 60,
-              child: RaisedButton(
-                color: Colors.redAccent,
-                onPressed: () {},
-                child: const Text('Custos',
-                    style: TextStyle(fontSize: 30, color: Colors.white)),
-              )),
-        ),
-        Padding(
-          padding: EdgeInsets.all(0.0),
-          child: SizedBox(
-              width: 250,
-              height: 60,
-              child: RaisedButton(
-                color: Colors.orange,
-                onPressed: () {},
-                child: const Text('Pessoal',
-                    style: TextStyle(fontSize: 30, color: Colors.white)),
-              )),
-        ),
-      ]));
+      margin:
+          EdgeInsets.only(top: 300.0, bottom: 300.0, left: 20.0, right: 20.0),
+      child: Scaffold(
+          backgroundColor: Color(0xFFFFFF),
+          body: ClipRRect(
+            borderRadius: BorderRadius.circular(20.0),
+            child: Column(
+              children: <Widget>[
+                Expanded(
+                    flex: 1,
+                    child: GestureDetector(
+                        onTap: () {
+                          print("It's Worked!");
+                        },
+                        child: Container(
+                            color: Colors.lightGreen,
+                            height: 100,
+                            width: 1000,
+                            child: Center(
+                                child: Text(
+                              "Adicionar Faturamento",
+                              textScaleFactor: 2.0,
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold),
+                            ))))),
+                Expanded(
+                    flex: 1,
+                    child: GestureDetector(
+                        onTap: () {
+                          print("It's Worked two!");
+                        },
+                        child: Container(
+                          color: Colors.red,
+                          height: 100,
+                            child: Center(
+                                child: Text(
+                                  "Adicionar Custo",
+                                  textScaleFactor: 2.0,
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold),
+                                ))
+                        ))),
+                Expanded(
+                    flex: 1,
+                    child: GestureDetector(
+                        onTap: () {
+                          print("It's Worked three!");
+                        },
+                        child: Container(
+                          color: Colors.orange,
+                          height: 100,
+                            child: Center(
+                                child: Text(
+                                  "Adicionar Despesa Pessoal",
+                                  textScaleFactor: 2.0,
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold),
+                                ))
+                        )))
+              ],
+            ),
+          )));
 
   // exibe o dialog
   showDialog(
