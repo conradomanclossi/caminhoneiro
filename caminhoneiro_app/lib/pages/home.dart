cf49d3d142e43c82e8badc25e6271fab72a6e8d1
Conrado <conrado.macalossi@gmail.com>
2019-09-20 23:01:34 -0300

    Home database improves

--- caminhoneiro_app/lib/pages/home.dart
+++ caminhoneiro_app/lib/pages/home.dart
@@ -1,6 +1,8 @@
 import 'package:caminhoneiro_app/sqlite/database.dart';
 import 'package:caminhoneiro_app/sqlite/suport_database.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
 
 class HomeApp extends StatefulWidget {
   @override
@@ -10,11 +12,19 @@ class HomeApp extends StatefulWidget {
 class _HomeAppState extends State<HomeApp> {
   ///Database
   DatabaseHelper helper = DatabaseHelper();
+  List<Categoria> categorias = List();
   List<Registro> registros = List();
 
   @override
   void initState() {
     super.initState();
+
+    helper.getAllCategorias().then((list) {
+      setState(() {
+        categorias = list;
+      });
+    });
+
     helper.getAllRegistros().then((list) {
       setState(() {
         registros = list;
@@ -35,8 +45,10 @@ class _HomeAppState extends State<HomeApp> {
 
   Widget _faturamentoCard(BuildContext context, int index) {
     return GestureDetector(
-      onTap: () {
-        print(registros[index].id);
+      onTap: () async {
+        print(await helper.getCategoria(registros[index].id).then((list) {
+          return list.titulo;
+        }));
       },
       child: Container(
         height: 70.0,
@@ -70,24 +82,25 @@ class _HomeAppState extends State<HomeApp> {
               ),
             ),
             Padding(
-                padding: EdgeInsets.only(left: 10.0),
-                child: Text.rich(
-                  TextSpan(
-                    text: registros[index].categoriaId.toString(),
-                    style: TextStyle(
-                        color: Colors.white70,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 10.0), // default text style
-                    children: <TextSpan>[
-                      TextSpan(
-                          text: registros[index].titulo,
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 25.0)),
-                    ],
-                  ),
-                )),
+              padding: EdgeInsets.only(left: 10.0),
+              child: Text.rich(
+                TextSpan(
+                  text: 'Categoria',
+                  style: TextStyle(
+                      color: Colors.white70,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 10.0), // default text style
+                  children: <TextSpan>[
+                    TextSpan(
+                        text: '\n${registros[index].titulo}',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 25.0)),
+                  ],
+                ),
+              ),
+            ),
             Align(
               alignment: Alignment.centerRight,
               child: Padding(
@@ -114,5 +127,11 @@ class _HomeAppState extends State<HomeApp> {
         ),
       ),
     );
+  }
+
+  Future<Categoria> _showCategoria(_id) async {
+    helper.getCategoria(_id).then((list) {
+      return list.titulo;
+    });
   }
 }
